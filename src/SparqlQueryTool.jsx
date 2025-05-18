d28238bb1d9e8910004123bfb8772ea59bcc1a70
Gonçalo Garcias <goncalogarcias@gmail.com>
2025-05-18 18:19:23 +0100

    Added an example query

--- src/SparqlQueryTool.jsx
+++ src/SparqlQueryTool.jsx
@@ -25,7 +25,30 @@ const exampleQueries = [
                 edu:degreeType ?degreeType ;
                 edu:availableSlots ?slots .
       }`
-  }
+  },
+  {
+    label: "Admission grade for Engenharia Eletrotécnica Marítima"
+    query: `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+      PREFIX edu: <http://example.org/education#>
+
+      SELECT DISTINCT ?institution ?course ?grade
+      WHERE {
+    
+        ?institution a edu:Institution;
+            edu:district "Lisboa"^^xsd:string;
+            edu:hasCourse ?course .
+          ?course rdf:type edu:Engenharia_Eletrotécnica_Marítima;
+              edu:lastAdmittedGrade ?grade;
+          FILTER(xsd:float(?grade) >= 110)
+    
+        # Filtros opcionais — ativa só os que precisares
+        # FILTER(CONTAINS(LCASE(?courseName), "engenharia"))
+        # FILTER(CONTAINS(LCASE(?location), "lisboa"))
+        # FILTER(xsd:float(?grade) >= 140)
+      }
+      LIMIT 10`
+    }
 ];
 
 const SparqlQueryTool = () => {
